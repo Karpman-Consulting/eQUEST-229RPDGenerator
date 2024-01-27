43c5b0cb198443d42b71fcdad5fb464c661f7611
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-01-26 23:35:38 -0500

    output parsed data to excel file

--- documentation/doe2 references/nhr_list_parse.py
+++ documentation/doe2 references/nhr_list_parse.py
@@ -1,9 +1,11 @@
 import re
 from itertools import islice
+from openpyxl import Workbook
 
-# UPDATE THE PATH TO THE NHR LIST FILE AS NEEDED
-# This is the path to the NHRList.txt file in the DOE2.3 v50e directory
+
+# This is the path to the NHRList.txt file in the DOE2.3 v50e directory. Update as needed.
 NHR_LIST_PATH = r'C:\doe23\EXE50e\NHRList.txt'
+OUTPUT_EXCEL_PATH = 'NHRList.xlsx'
 
 HEADER_REGEX = r'\d{4}\s{3}[A-Z]{2,}'
 LINE_ITEM_REGEX = r'^\s{5}\d{7},\s{2}'
@@ -26,25 +28,32 @@ def parse_header(line):
     return [id_front4, report_name, report_desc, prim_report_id]
 
 
-def parse_line_item(line):
+def parse_line_item(line, current_header):
     entry_id = line[5:12]
     report_name = line[78:].strip(" \n")
 
-    return [entry_id, report_name]
+    return [entry_id, report_name, current_header]
 
 
 def process_file(file_path, header_regex, line_item_regex):
+    wb = Workbook()
+    ws = wb.active
+
+    current_header = None
+
     with open(file_path, 'r') as file:
         for line in islice(file, 192, None):  # Skip first 192 lines
-
             # Check if line is a header
             if re.search(header_regex, line):
-                parsed_data = parse_header(line)
+                current_header = parse_header(line)
 
             # Check if line is a line item
-            elif re.search(line_item_regex, line):
-                parsed_data = parse_line_item(line)
+            elif re.search(line_item_regex, line) and current_header is not None:
+                parsed_data = parse_line_item(line, current_header[1])
+                ws.append(parsed_data)
 
+    # Save to Excel
+    wb.save(OUTPUT_EXCEL_PATH)
 
 
 process_file(NHR_LIST_PATH, HEADER_REGEX, LINE_ITEM_REGEX)
