0a7557d4bf14edab14fce6c44e9ce3a8135229e3
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-01-29 13:41:42 -0500

    correct instantiation and parent inheritance

--- rpd_generator/doe2_file_readers/model_input_reader.py
+++ rpd_generator/doe2_file_readers/model_input_reader.py
@@ -1,55 +1,136 @@
 import inspect
 import pkgutil
 import importlib
-from rpd_generator.models import *
+from rpd_generator.bdl_structure import *
 
 
-def read_input_bdl_file(bdl_file_path: str) -> dict:
-    """
-    Read BDL input file and return dictionary of objects.
-    """
-    bdl_command_dict = get_bdl_commands_for_rpd()
-    with open(bdl_file_path, "r") as bdl_file:
-        instance_dict = {cmd: [] for cmd in bdl_command_dict}
-        previous_line = ""
-        parent = None
-        for line in bdl_file:
-            # TODO: Implement a definitive way to know if the line is a bdl command. Parameter catch may not be enough.
-            if '" = ' in line and "PARAMETER" not in previous_line:
-                unique_name, command = (part.strip() for part in line.split('" = '))
-                unique_name = unique_name.split('"')[1]
-                if command in bdl_command_dict:
-                    bdl_command = bdl_command_dict[command]
-                    if issubclass(bdl_command, ChildNode):
-                        obj = bdl_command(unique_name, parent)
-                        instance_dict[command].append(obj)
-                    elif issubclass(bdl_command, ParentNode) or issubclass(
-                            bdl_command, ParentDefinition
-                    ):
-                        obj = bdl_command(unique_name)
-                        instance_dict[command].append(obj)
-                        # store most recent parent instance in memory to assign to next child instance(s)
-                        parent = obj
-                    else:
-                        obj = bdl_command(unique_name)
-                        instance_dict[command].append(obj)
-            previous_line = line
-    return instance_dict
+class ModelInputReader:
+    """Model input reader class."""
+
+    def __init__(self):
+        self.bdl_command_dict = self._get_bdl_commands_for_rpd()
+        self.current_parent_floor = None
+        self.current_parent_space = None
+        self.current_parent = None
+
+    def read_input_bdl_file(self, bdl_file_path):
+        """
+        Read BDL input file and return a dictionary of object instances.
+
+        Args:
+            bdl_file_path (str): Path to the BDL file.
+
+        Returns:
+            dict: A dictionary with BDL commands as keys and lists of their respective instances as values.
+        """
+        with open(bdl_file_path, "r") as bdl_file:
+            command_instances = {cmd: [] for cmd in self.bdl_command_dict}
+            previous_line = ""
+
+            for line in bdl_file:
+                # TODO: Implement a definitive way to know if the line is a bdl command. Parameter catch may not be enough.
+                if '" = ' in line and "PARAMETER" not in previous_line:
+                    unique_name, command = self._parse_line(line)
+                    if command in self.bdl_command_dict:
+                        obj_instance = self._create_obj_instance(unique_name, command)
+                        command_instances[command].append(obj_instance)
+
+                previous_line = line
+
+            return command_instances
+
+    def _set_current_parent(self, obj):
+        if isinstance(obj, self.bdl_command_dict["FLOOR"]):
+            self.current_parent_floor = obj
+        elif isinstance(obj, self.bdl_command_dict["SPACE"]):
+            self.current_parent_space = obj
+        else:
+            self.current_parent = obj
+
+    def _create_obj_instance(self, unique_name, command):
+        """
+        Create an object instance based on the command type.
+
+        Args:
+            unique_name (str): Unique name for the instance.
+            command (str): BDL command.
+
+        Returns:
+            Object: Created object instance.
+        """
+        command_class = self.bdl_command_dict[command]
+        is_parent, is_child, is_int_ext_wall, is_space = self._determine_instance_type(command_class)
+
+        if is_int_ext_wall:
+            obj_instance = command_class(unique_name, self.current_parent_space)
+            self._set_current_parent(obj_instance)
+        elif is_space:
+            obj_instance = command_class(unique_name, self.current_parent_floor)
+            self._set_current_parent(obj_instance)
+        elif is_child:
+            obj_instance = command_class(unique_name, self.current_parent)
+        elif is_parent:
+            obj_instance = command_class(unique_name)
+            self._set_current_parent(obj_instance)
+        else:
+            obj_instance = command_class(unique_name)
+
+        return obj_instance
+
+    @staticmethod
+    def _parse_line(line):
+        """
+        Parse the line to extract unique name and command.
+
+        Args:
+            line (str): Line to be parsed.
+
+        Returns:
+            tuple: Unique name and command extracted from the line.
+        """
+        parts = line.split('" = ')
+        unique_name = parts[0].strip().split('"')[1]
+        command = parts[1].strip()
+        return unique_name, command
+
+    @staticmethod
+    def _get_bdl_commands_for_rpd() -> dict:
+        """
+        Return a dictionary of BDL commands mapped to their respective class objects to facilitate instantiation.
+        """
+        commands_dict = {}
+        prefix = bdl_commands.__name__ + "."
+        # iterate through the modules in the bdl_commands package
+        for _, name, _ in pkgutil.iter_modules(bdl_commands.__path__, prefix):
+            module = importlib.import_module(name)
+            # iterate through the classes in each module
+            for _, obj in inspect.getmembers(module, inspect.isclass):
+                # if the class is a subclass of BaseNode or BaseDefinition
+                if issubclass(obj, (BaseNode, BaseDefinition)):
+                    # get the bdl_command attribute
+                    bdl_command = getattr(obj, "bdl_command", None)
+                    # insert the bdl_command and class object into the dictionary to facilitate instantiation
+                    commands_dict[bdl_command] = obj
+        return commands_dict
+
+    @staticmethod
+    def _determine_instance_type(command_class):
+        """
+        Determine the type of BDL command class.
+
+        Args:
+            command_class (class): Command class to be determined.
+
+        Returns:
+            tuple: Indicates if the class is parent, child, int_ext_wall, or space.
+        """
+        is_parent = issubclass(command_class, ParentNode) or issubclass(command_class, ParentDefinition)
+        is_child = issubclass(command_class, ChildNode)
+        is_int_ext_wall = issubclass(command_class, ParentNode) and is_child
+        is_space = issubclass(command_class, ParentDefinition) and is_child
+
+        return is_parent, is_child, is_int_ext_wall, is_space
 
 
-def get_bdl_commands_for_rpd() -> dict:
-    """
-    Return a dictionary of BDL commands mapped to their respective class objects.
-    """
-    commands_dict = {}
-    prefix = bdl_commands.__name__ + "."
-    for _, name, _ in pkgutil.iter_modules(bdl_commands.__path__, prefix):
-        module = importlib.import_module(name)
-        for _, obj in inspect.getmembers(module, inspect.isclass):
-            if issubclass(obj, (BaseNode, BaseDefinition)):
-                bdl_command = getattr(obj, "bdl_command", None)
-                commands_dict[bdl_command] = obj
-    return commands_dict
-
-
-print(read_input_bdl_file(r"../../test/example/INP.BDL"))
+reader = ModelInputReader()
+model_object_structure = reader.read_input_bdl_file(r"../../test/example/INP.BDL")
