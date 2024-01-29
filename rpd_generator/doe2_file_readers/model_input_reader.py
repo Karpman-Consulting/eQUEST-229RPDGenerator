b2755e7726eb9f5894f9a897587ea0396bd12b1f
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-01-28 22:34:59 -0500

    parse bdl commands to objects

--- rpd_generator/doe2_file_readers/model_input_reader.py
+++ rpd_generator/doe2_file_readers/model_input_reader.py
@@ -4,30 +4,47 @@ import importlib
 from rpd_generator.models import *
 
 
-def read_bdl_input_file(bdl_file_path: str) -> dict:
-    """Read BDL input file and return dictionary of objects."""
+def read_bdl_input_file(bdl_file_path: str) -> None:
+    """
+    Read BDL input file and return dictionary of objects.
+    """
+    bdl_commands_dict = get_bdl_commands_for_rpd()
+    parent = None
+    with open(bdl_file_path, "r") as bdl_file:
+        previous_line = ""
+        for line in bdl_file:
+            # TODO: Implement a definitive way to know if the line is a bdl command. Parameter catch may not be enough.
+            if '" = ' in line and "PARAMETER" not in previous_line:
+                unique_name, command = (part.strip() for part in line.split('" = '))
+                unique_name = unique_name.split('"')[1]
+                if command in bdl_commands_dict:
 
-    bdl_commands_for_rpd = get_bdl_commands_for_rpd()
-    print(bdl_commands_for_rpd)
-    # with open(bdl_file_path, "r") as bdl_file:
-    # for line in bdl_file:
-    #     print(line)
-    return {}
+                    bdl_command = bdl_commands_dict[command]
+                    if issubclass(bdl_command, ChildNode):
+                        obj = bdl_command(unique_name, parent)
+                    elif issubclass(bdl_command, ParentNode) or issubclass(
+                        bdl_command, ParentDefinition
+                    ):
+                        obj = bdl_command(unique_name)
+                        parent = obj
+                    else:
+                        obj = bdl_command(unique_name)
+            previous_line = line
 
 
-def get_bdl_commands_for_rpd():
-    commands = []
+def get_bdl_commands_for_rpd() -> dict:
+    """
+    Return a dictionary of BDL commands mapped to their respective class objects.
+    """
+    commands_dict = {}
     prefix = bdl_commands.__name__ + "."
-    for finder, name, is_pkg in pkgutil.iter_modules(bdl_commands.__path__, prefix):
+    for _, name, _ in pkgutil.iter_modules(bdl_commands.__path__, prefix):
         module = importlib.import_module(name)
-        for mod_name, obj in inspect.getmembers(module):
-            if (
-                inspect.isclass(obj)
-                and (issubclass(obj, BaseNode) or issubclass(obj, BaseDefinition))
-                and obj not in [BaseNode, BaseDefinition, ParentNode, ChildNode]
-            ):
-                commands.append(getattr(obj, "bdl_command", None))
-    return commands
+        for _, obj in inspect.getmembers(module, inspect.isclass):
+            if issubclass(obj, (BaseNode, BaseDefinition)):
+                bdl_command = getattr(obj, "bdl_command", None)
+                commands_dict[bdl_command] = obj
+    return commands_dict
 
 
 read_bdl_input_file(r"../../test/example/INP.BDL")
