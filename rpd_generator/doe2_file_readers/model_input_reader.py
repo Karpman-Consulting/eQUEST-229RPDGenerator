60515aa1b796c8790e95fd9380ff658663fea0c7
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-01-28 23:20:26 -0500

    populate object instance dict

--- rpd_generator/doe2_file_readers/model_input_reader.py
+++ rpd_generator/doe2_file_readers/model_input_reader.py
@@ -4,32 +4,37 @@ import importlib
 from rpd_generator.models import *
 
 
-def read_bdl_input_file(bdl_file_path: str) -> None:
+def read_input_bdl_file(bdl_file_path: str) -> dict:
     """
     Read BDL input file and return dictionary of objects.
     """
-    bdl_commands_dict = get_bdl_commands_for_rpd()
-    parent = None
+    bdl_command_dict = get_bdl_commands_for_rpd()
     with open(bdl_file_path, "r") as bdl_file:
+        instance_dict = {cmd: [] for cmd in bdl_command_dict}
         previous_line = ""
+        parent = None
         for line in bdl_file:
             # TODO: Implement a definitive way to know if the line is a bdl command. Parameter catch may not be enough.
             if '" = ' in line and "PARAMETER" not in previous_line:
                 unique_name, command = (part.strip() for part in line.split('" = '))
                 unique_name = unique_name.split('"')[1]
-                if command in bdl_commands_dict:
-
-                    bdl_command = bdl_commands_dict[command]
+                if command in bdl_command_dict:
+                    bdl_command = bdl_command_dict[command]
                     if issubclass(bdl_command, ChildNode):
                         obj = bdl_command(unique_name, parent)
+                        instance_dict[command].append(obj)
                     elif issubclass(bdl_command, ParentNode) or issubclass(
-                        bdl_command, ParentDefinition
+                            bdl_command, ParentDefinition
                     ):
                         obj = bdl_command(unique_name)
+                        instance_dict[command].append(obj)
+                        # store most recent parent instance in memory to assign to next child instance(s)
                         parent = obj
                     else:
                         obj = bdl_command(unique_name)
+                        instance_dict[command].append(obj)
             previous_line = line
+    return instance_dict
 
 
 def get_bdl_commands_for_rpd() -> dict:
@@ -47,4 +52,4 @@ def get_bdl_commands_for_rpd() -> dict:
     return commands_dict
 
 
-read_bdl_input_file(r"../../test/example/INP.BDL")
+print(read_input_bdl_file(r"../../test/example/INP.BDL"))
