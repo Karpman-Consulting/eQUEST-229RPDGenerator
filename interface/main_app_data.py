9d30d92d7fc581a886d65e735a95ded649202755
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-09 21:34:26 -0500

    improve file validation

--- interface/main_app_data.py
+++ interface/main_app_data.py
@@ -31,18 +31,17 @@ class MainAppData:
                 # Check if all target files are in the current directory in a case-insensitive manner
                 if all(file in filenames_lower for file in target_files):
                     self.installation_path.set(dirpath)
-                    config = configparser.ConfigParser()
-                    config.read(os.path.join(dirpath, "eQUEST.ini"))
-                    self.doe22_data_path = config.get('paths', 'DataPath').strip('"')
-                    config.read(os.path.join(dirpath, "eQUESTD23.ini"))
-                    self.doe23_data_path = config.get('paths', 'DataPath').strip('"')
+                    self.set_data_path_from_config()
 
     def verify_equest_installation(self):
-        installation_path = self.installation_path.get()
+        install_path = self.installation_path.get()
+        error_message = self.set_data_path_from_config()
+        if error_message is not None:
+            return error_message
 
         # List of files to check
-        files_to_check = [os.path.join(installation_path, "BDLCIO32.DLL"),
-                          os.path.join(installation_path, "D2Result.dll"),
+        files_to_check = [os.path.join(install_path, "BDLCIO32.DLL"),
+                          os.path.join(install_path, "D2Result.dll"),
                           os.path.join(self.doe22_data_path, "DOE-2\\eQ_Lib.dat"),
                           os.path.join(self.doe23_data_path, "DOE23\\eQ_Lib.dat"),
                           os.path.join(self.doe22_data_path, "DOE-2\\BDLLIB.DAT"),
@@ -52,8 +51,25 @@ class MainAppData:
 
         # Check each file and raise an error if it doesn't exist
         for file_path in files_to_check:
-            if not os.path.exists(file_path):
-                print(f"Required file does not exist: {file_path}")
-                self.files_verified = False
+            if not os.path.exists(file_path) and error_message is None:
+                error_message = f"File not found: {file_path}\n"
+            elif not os.path.exists(file_path):
+                error_message += f"File not found: {file_path}\n"
 
-        self.files_verified = True
+        if error_message is None:
+            self.files_verified = True
+
+        return error_message
+
+    def set_data_path_from_config(self):
+        install_path = self.installation_path.get()
+        doe22_ini_path = os.path.join(install_path, "eQUEST.ini")
+        doe23_ini_path = os.path.join(install_path, "eQUESTD23.ini")
+        if not os.path.exists(doe22_ini_path) or not os.path.exists(doe23_ini_path):
+            return f"{doe22_ini_path} or {doe23_ini_path} not found. Please verify the installation path."
+        config = configparser.ConfigParser()
+        config.read(doe22_ini_path)
+        self.doe22_data_path = config.get('paths', 'DataPath').strip('"')
+        config.read(doe23_ini_path)
+        self.doe23_data_path = config.get('paths', 'DataPath').strip('"')
+        return None
