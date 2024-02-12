9e92b15dd9b82ad080e39d10cd9ae8387ec6f0f9
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-12 15:53:32 -0500

    simplified data structure

--- rpd_generator/bdl_structure/bdl_commands/circulation_loop.py
+++ rpd_generator/bdl_structure/bdl_commands/circulation_loop.py
@@ -12,26 +12,21 @@ class CirculationLoop(BaseNode):
         # keep track of the type of circulation loop (different from self.type which is the FluidLoop type)
         self.circulation_loop_type = None
 
-        # FluidLoop data
-        self.fluid_loop_data_structure = {}
+        # Initialize the data structure for the different types of circulation loops
+        self.data_structure = {}
 
-        # data elements with children
+        # FluidLoop data elements with children
         self.cooling_or_condensing_design_and_control = {}
         self.heating_design_and_control = {}
         self.child_loops = []
-
-        # data elements with no children
+        # FluidLoop data elements with no children
         self.type = None
         self.pump_power_per_flow_rate = None
 
-        # ServiceWaterHeatingDistributionSystem data
-        self.swh_distribution_data_structure = {}
-
-        # data elements with children
+        # ServiceWaterHeatingDistributionSystem data elements with children
         self.service_water_piping = None
         self.tanks = None
-
-        # data elements with no children
+        # ServiceWaterHeatingDistributionSystem data elements with no children
         self.design_supply_temperature = None
         self.design_supply_temperature_difference = None
         self.is_central_system = None
@@ -45,17 +40,14 @@ class CirculationLoop(BaseNode):
         self.entering_water_mains_temperature_schedule = None
         self.is_ground_temperature_used_for_entering_water = None
 
-        # ServiceWaterPiping data
-        self.swh_piping_data_structure = {}
-
-        # data elements with no children
+        # ServiceWaterPiping data elements with no children
         self.is_recirculation_loop = None
         self.insulation_thickness = None
         self.loop_pipe_location = None
         self.location_zone = None
         self.length = None
         self.diameter = None
-        # self.child = None   this is commented out because eQUEST can only have a single tier of secondary loops
+        # self.child = None   this is commented out because in eQUEST every secondary loop is a child of a primary loop
 
     def __repr__(self):
         return f"CirculationLoop(u_name='{self.u_name}')"
@@ -65,27 +57,29 @@ class CirculationLoop(BaseNode):
         self.determine_circ_loop_type()
 
     def populate_data_group(self):
-        """Populate schema structure for circulation loop object."""
-        if (
-            self.keyword_value_pairs["TYPE"] == "DHW"
-            and self.keyword_value_pairs["SUBTYPE"] == "SECONDARY"
-        ):
-            self.swh_piping_data_structure = {
+        """Populate schema structure for circulation loop object. These data elements will be used in every instance of
+        circulation loop. Other data elements may or may not be present depending on the model"""
+        self.circulation_loop_type = self.determine_circ_loop_type()
+
+        if self.circulation_loop_type == "ServiceWaterPiping":
+            self.data_structure = {
                 "id": self.u_name,
             }
-        elif self.keyword_value_pairs["TYPE"] == "DHW":
-            self.swh_distribution_data_structure = {
+        elif self.circulation_loop_type == "ServiceWaterHeatingDistributionSystem":
+            self.data_structure = {
                 "id": self.u_name,
                 "tanks": self.tanks,
                 "service_water_piping": self.service_water_piping,
             }
         else:
-            self.fluid_loop_data_structure = {
+            self.data_structure = {
                 "id": self.u_name,
                 "cooling_or_condensing_design_and_control": self.cooling_or_condensing_design_and_control,
                 "heating_design_and_control": self.heating_design_and_control,
                 "child_loops": self.child_loops,
             }
+
+        self.populate_data_elements()
 
         no_children_attributes = [
             "reporting_name",
@@ -112,44 +106,39 @@ class CirculationLoop(BaseNode):
             "diameter",
         ]
 
-        structure_map = {
-            "FluidLoop": self.fluid_loop_data_structure,
-            "ServiceWaterHeatingDistributionSystem": self.swh_distribution_data_structure,
-            "ServiceWaterPiping": self.swh_piping_data_structure,
-        }
-
         # Iterate over the no_children_attributes list and populate if the value is not None
         for attr in no_children_attributes:
             value = getattr(self, attr, None)
             if value is not None:
-                data_structure = structure_map.get(self.circulation_loop_type)
-                data_structure[attr] = value
+                self.data_structure[attr] = value
 
     def insert_to_rpd(self, rmd):
-        self.determine_circ_loop_type()
         if self.circulation_loop_type == "FluidLoop":
-            rmd.fluid_loops.append(self.fluid_loop_data_structure)
+            rmd.fluid_loops.append(self.data_structure)
         elif self.circulation_loop_type == "SecondaryFluidLoop":
             primary_loop = self.keyword_value_pairs.get("PRIMARY-LOOP")
             for fluid_loop in rmd.fluid_loops:
                 if fluid_loop["id"] == primary_loop:
-                    fluid_loop["child_loops"].append(self.fluid_loop_data_structure)
+                    fluid_loop["child_loops"].append(self.data_structure)
         elif self.circulation_loop_type == "ServiceWaterHeatingDistributionSystem":
             rmd.service_water_heating_distribution_systems.append(
-                self.swh_distribution_data_structure
+                self.data_structure
             )
         elif self.circulation_loop_type == "ServiceWaterPiping":
             primary_loop = self.keyword_value_pairs.get("PRIMARY-LOOP")
+            for fluid_loop in rmd.fluid_loops:
+                if fluid_loop["id"] == primary_loop:
+                    fluid_loop["child_loops"].append(self.data_structure)
 
     def determine_circ_loop_type(self):
         if (
-            self.keyword_value_pairs["TYPE"] == "DHW"
-            and self.keyword_value_pairs["SUBTYPE"] == "SECONDARY"
+                self.keyword_value_pairs["TYPE"] == "DHW"
+                and self.keyword_value_pairs["SUBTYPE"] == "SECONDARY"
         ):
-            self.circulation_loop_type = "ServiceWaterPiping"
+            return "ServiceWaterPiping"
         elif self.keyword_value_pairs["TYPE"] == "DHW":
-            self.circulation_loop_type = "ServiceWaterHeatingDistributionSystem"
+            return "ServiceWaterHeatingDistributionSystem"
         elif self.keyword_value_pairs.get("PRIMARY-LOOP") is None:
-            self.circulation_loop_type = "FluidLoop"
+            return "FluidLoop"
         else:
-            self.circulation_loop_type = "SecondaryFluidLoop"
+            return "SecondaryFluidLoop"
