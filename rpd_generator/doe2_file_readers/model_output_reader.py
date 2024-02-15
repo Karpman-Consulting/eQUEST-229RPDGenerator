50ae6f96e490b819cde78068b64c38025cac5458
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-15 18:51:57 -0500

    enhance get multiple results

--- rpd_generator/doe2_file_readers/model_output_reader.py
+++ rpd_generator/doe2_file_readers/model_output_reader.py
@@ -45,48 +45,54 @@ pMRTs: Pointer to an array of MultResultsType structures:
         {
            int  iEntryID;      // from NHRList.txt
            int  iReturnValue;  // success/failure
-           char pszReportKey[ 34 ];
-           char pszRowKey[    34 ];
+           char pszReportKey[ 40 ];
+           char pszRowKey[    40 ];
         };
 """
 
 
-def get_multiple_results(d2_result_dll, doe2_dir, project_fname, entry_id):
+def get_multiple_results(d2_result_dll, doe2_data_dir, project_fname, request_array):
     """
     Get Multiple Results from the simulation output files
     :param d2_result_dll: (string) path to user's eQUEST D2Result.dll file included with installation files
-    :param doe2_dir: (binary string) path to DOE-2 data directory with NHRList.txt
+    :param doe2_data_dir: (binary string) path to DOE-2 data directory with NHRList.txt
     :param project_fname: (binary string) path to project with project name NOT INCLUDING FILE EXTENSION
-    :param entry_id: (int) id from NHRList.txt corresponding to the value to retrieve
+    :param request_array: (list) list of entry_id: (int) from NHRList.txt corresponding to the value to retrieve,
+    report_key: (binary string) to use when RI > 0 and when value to retrieve refers to a particular BDL component,
+    and row_key: (binary string) to use when KT > 0 and when a report has multiple row where each row provides results for a separate building component or month of the year
     :return: list of returned values from the binary simulation output files
     """
     d2_result_dll = ctypes.CDLL(d2_result_dll)
     multiple_result_dll = d2_result_dll.D2R_GetMultipleResult
+    nhr_list_path = doe2_data_dir + rb"\NHRList.txt"
 
-    file_type = 1
-    pf_data = (ctypes.c_float * 13)()
-    max_values = 13
-    num_mrts = 1
-
-    class MRTArray(ctypes.Structure):
-        _fields_ = [
-            ("entry_id", ctypes.c_int),
-            ("return_value", ctypes.c_int),
-            ("psz_report_key", ctypes.c_char * 34),
-            ("psz_row_key", ctypes.c_char * 34),
-        ]
-
-    mrt = MRTArray()
+    num_mrts = len(request_array)
     mrt_array = (MRTArray * num_mrts)()
+    if len(request_array) > 0:
+        file_type = int(str(request_array[0][0])[0]) - 1
+    else:
+        return []
 
-    mrt_array[0].entry_id = entry_id
-    mrt_array[0].psz_report_key = b"EM1"
-    mrt_array[0].psz_row_key = b"\0"
+    max_values = 0
+    for i, value_request in enumerate(request_array):
+        entry_id, report_key, row_key = value_request
 
-    p_mrts = mrt_array
+        mrt_array[i].entry_id = entry_id
+        mrt_array[i].psz_report_key = report_key
+        mrt_array[i].psz_row_key = row_key
+
+        with open(nhr_list_path, "r") as nhr_list:
+            for line in nhr_list:
+                parts = [part.strip() for part in line.split(",")]
+                if parts[0] == str(entry_id):
+                    # Figure out the max_values for the value request (NI in NHRList.txt) and add to the total
+                    max_values += int(parts[6])
+                    break
+
+    pf_data = (ctypes.c_float * max_values)()
 
     multiple_result_dll(
-        doe2_dir, project_fname, file_type, pf_data, max_values, num_mrts, p_mrts
+        doe2_data_dir, project_fname, file_type, pf_data, max_values, num_mrts, mrt_array
     )
 
     return [data for data in pf_data]
