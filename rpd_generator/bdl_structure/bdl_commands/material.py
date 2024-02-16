c6af94d9f9de65c7dcd80ab65d978fc8c1977a3d
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-15 21:32:14 -0500

    populate material data elements

--- rpd_generator/bdl_structure/bdl_commands/material.py
+++ rpd_generator/bdl_structure/bdl_commands/material.py
@@ -21,6 +21,28 @@ class Material(BaseNode):
     def __repr__(self):
         return f"Material(u_name='{self.u_name}')"
 
+    def populate_data_elements(self):
+        """Populate data elements for material object."""
+        self.thickness = self.keyword_value_pairs.get("THICKNESS")
+        if self.thickness is not None:
+            self.thickness = float(self.thickness)
+
+        self.thermal_conductivity = self.keyword_value_pairs.get("CONDUCTIVITY")
+        if self.thermal_conductivity is not None:
+            self.thermal_conductivity = float(self.thermal_conductivity)
+
+        self.density = self.keyword_value_pairs.get("DENSITY")
+        if self.density is not None:
+            self.density = float(self.density)
+
+        self.specific_heat = self.keyword_value_pairs.get("SPECIFIC-HEAT")
+        if self.specific_heat is not None:
+            self.specific_heat = float(self.specific_heat)
+
+        self.r_value = self.keyword_value_pairs.get("RESISTANCE")
+        if self.r_value is not None:
+            self.r_value = float(self.r_value)
+
     def populate_data_group(self):
         """Populate schema structure for material object."""
         self.material_data_structure = {
@@ -41,7 +63,7 @@ class Material(BaseNode):
         for attr in no_children_attributes:
             value = getattr(self, attr, None)
             if value is not None:
-                self.material_data_structure[self.u_name][attr] = value
+                self.material_data_structure[attr] = value
 
     def insert_to_rpd(self, rmd):
         """Insert window object into the rpd data structure."""
