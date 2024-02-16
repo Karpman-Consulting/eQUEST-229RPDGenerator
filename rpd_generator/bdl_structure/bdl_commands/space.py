ee8b089f3f9ee3f33d573b6ac692c2acb2d14270
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-15 21:15:56 -0500

    black format

--- rpd_generator/bdl_structure/bdl_commands/space.py
+++ rpd_generator/bdl_structure/bdl_commands/space.py
@@ -44,7 +44,9 @@ class Space(ChildNode, ParentNode):
         if self.number_of_occupants is not None:
             self.number_of_occupants = float(self.number_of_occupants)
 
-        self.occupant_sensible_heat_gain = self.keyword_value_pairs.get("PEOPLE-HG-SENS")
+        self.occupant_sensible_heat_gain = self.keyword_value_pairs.get(
+            "PEOPLE-HG-SENS"
+        )
         if self.occupant_sensible_heat_gain is not None:
             self.occupant_sensible_heat_gain = float(self.occupant_sensible_heat_gain)
 
