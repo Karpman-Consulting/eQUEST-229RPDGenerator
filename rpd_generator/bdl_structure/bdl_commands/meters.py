db5d15b8ef8764a0535738169b1a002fb1d20ffb
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-16 20:58:10 -0500

    created meter classes

--- rpd_generator/bdl_structure/bdl_commands/meters.py
+++ rpd_generator/bdl_structure/bdl_commands/meters.py
@@ -1,1 +1,76 @@
 from rpd_generator.bdl_structure.base_definition import BaseDefinition
+
+
+class MasterMeters(BaseDefinition):
+
+    bdl_command = "MASTER-METERS"
+
+    def __init__(self, u_name):
+        super().__init__(u_name)
+
+    def __repr__(self):
+        return f"MasterMeters(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements from master meters object."""
+        return {}
+
+
+class FuelMeter(BaseDefinition):
+
+    bdl_command = "FUEL-METER"
+
+    def __init__(self, u_name):
+        super().__init__(u_name)
+
+    def __repr__(self):
+        return f"FuelMeter(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements from fuel meter object."""
+        return {}
+
+
+class ElecMeter(BaseDefinition):
+
+    bdl_command = "ELEC-METER"
+
+    def __init__(self, u_name):
+        super().__init__(u_name)
+
+    def __repr__(self):
+        return f"ElecMeter(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements from electric meter object."""
+        return {}
+
+
+class SteamMeter(BaseDefinition):
+
+    bdl_command = "STEAM-METER"
+
+    def __init__(self, u_name):
+        super().__init__(u_name)
+
+    def __repr__(self):
+        return f"SteamMeter(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements from steam meter object."""
+        return {}
+
+
+class CHWMeter(BaseDefinition):
+
+    bdl_command = "CHW-METER"
+
+    def __init__(self, u_name):
+        super().__init__(u_name)
+
+    def __repr__(self):
+        return f"CHWMeter(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements from chilled water meter object."""
+        return {}
