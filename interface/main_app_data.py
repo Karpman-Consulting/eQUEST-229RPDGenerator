a40e1f34563bbbd347462d325ace6c2766ec9779
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-02-20 00:19:10 -0500

    use pathlib

--- interface/main_app_data.py
+++ interface/main_app_data.py
@@ -1,6 +1,10 @@
-import customtkinter as ctk
-import configparser
 import os
+import configparser
+import customtkinter as ctk
+from pathlib import Path
+from rpd_generator import main as rpd_generator
+from rpd_generator.doe2_file_readers.bdlcio32 import process_input_file
+from rpd_generator.doe2_file_readers.model_input_reader import ModelInputReader
 
 
 class MainAppData:
@@ -13,6 +17,8 @@ class MainAppData:
 
         self.files_verified = False
 
+        self.test_inp_path = ctk.StringVar()
+
     def find_equest_installation(self):
         """
         Search recursively for a directory that matches a target directory starting from start_path.
@@ -20,44 +26,49 @@ class MainAppData:
 
         :return: Path of the directory if it exists with all target files, else None.
         """
-        start_path = os.environ.get("ProgramFiles(x86)", "C:/")
+        start_path = Path(os.environ.get("ProgramFiles(x86)", "C:/"))
         target_dir = "eQUEST 3-65-7175".lower()  # Convert target directory to lowercase
         target_files = [
-            file.lower() for file in ["D2RESULT.DLL", "BDLCIO32.DLL"]
-        ]  # Convert target files to lowercase
-        for dirpath, dirnames, filenames in os.walk(start_path):
-            # Convert the filenames in current directory to lowercase for case-insensitive comparison
-            filenames_lower = [file.lower() for file in filenames]
-            # Check if current directory name matches target directory in a case-insensitive manner
-            if os.path.basename(dirpath).lower() == target_dir:
-                # Check if all target files are in the current directory in a case-insensitive manner
-                if all(file in filenames_lower for file in target_files):
-                    self.installation_path.set(dirpath)
+            "d2result.dll",
+            "bdlcio32.dll",
+        ]  # Converted target files to lowercase for comparison
+
+        for path in start_path.rglob("*"):
+            # Check if the current path is a directory and matches the target directory name (case-insensitive)
+            if path.is_dir() and path.name.lower() == target_dir:
+                # List all files in the current directory (case-insensitive match)
+                files_in_dir = [
+                    file.name.lower() for file in path.iterdir() if file.is_file()
+                ]
+                # Check if all target files exist in the current directory (case-insensitive)
+                if all(file in files_in_dir for file in target_files):
+                    self.installation_path.set(str(path))
                     self.set_data_path_from_config()
+                    return
 
     def verify_equest_installation(self):
-        install_path = self.installation_path.get()
+        install_path = Path(self.installation_path.get())
         error_message = self.set_data_path_from_config()
         if error_message is not None:
             return error_message
 
         # List of files to check
         files_to_check = [
-            os.path.join(install_path, "BDLCIO32.DLL"),
-            os.path.join(install_path, "D2Result.dll"),
-            os.path.join(self.doe22_data_path, "DOE-2\\eQ_Lib.dat"),
-            os.path.join(self.doe23_data_path, "DOE23\\eQ_Lib.dat"),
-            os.path.join(self.doe22_data_path, "DOE-2\\BDLLIB.DAT"),
-            os.path.join(self.doe23_data_path, "DOE23\\BDLLIB.DAT"),
-            os.path.join(self.doe22_data_path, "DOE-2\\Usrlib.dat"),
-            os.path.join(self.doe23_data_path, "DOE23\\Usrlib.dat"),
+            install_path / "BDLCIO32.DLL",
+            install_path / "D2Result.dll",
+            Path(self.doe22_data_path) / "DOE-2" / "eQ_Lib.dat",
+            Path(self.doe23_data_path) / "DOE23" / "eQ_Lib.dat",
+            Path(self.doe22_data_path) / "DOE-2" / "BDLLIB.DAT",
+            Path(self.doe23_data_path) / "DOE23" / "BDLLIB.DAT",
+            Path(self.doe22_data_path) / "DOE-2" / "Usrlib.dat",
+            Path(self.doe23_data_path) / "DOE23" / "Usrlib.dat",
         ]
 
         # Check each file and raise an error if it doesn't exist
         for file_path in files_to_check:
-            if not os.path.exists(file_path) and error_message is None:
+            if not file_path.exists() and error_message is None:
                 error_message = f"File not found: {file_path}\n"
-            elif not os.path.exists(file_path):
+            elif not file_path.exists():
                 error_message += f"File not found: {file_path}\n"
 
         if error_message is None:
@@ -66,10 +77,10 @@ class MainAppData:
         return error_message
 
     def set_data_path_from_config(self):
-        install_path = self.installation_path.get()
-        doe22_ini_path = os.path.join(install_path, "eQUEST.ini")
-        doe23_ini_path = os.path.join(install_path, "eQUESTD23.ini")
-        if not os.path.exists(doe22_ini_path) or not os.path.exists(doe23_ini_path):
+        install_path = Path(self.installation_path.get())
+        doe22_ini_path = install_path / "eQUEST.ini"
+        doe23_ini_path = install_path / "eQUESTD23.ini"
+        if not doe22_ini_path.exists() or not doe23_ini_path.exists():
             return f"{doe22_ini_path} or {doe23_ini_path} not found. Please verify the installation path."
         config = configparser.ConfigParser()
         config.read(doe22_ini_path)
