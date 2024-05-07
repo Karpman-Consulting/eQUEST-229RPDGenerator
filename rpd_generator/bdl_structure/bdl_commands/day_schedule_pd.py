07841c7b468c5cc17f58871a3a4cc2f6c214adb2
Christina LaPerle <christina@karpmanconsulting.net>
2024-05-07 10:43:25 -0400

    Week and day py changes

--- rpd_generator/bdl_structure/bdl_commands/day_schedule_pd.py
+++ rpd_generator/bdl_structure/bdl_commands/day_schedule_pd.py
@@ -12,9 +12,13 @@ class DaySchedulePD(BaseDefinition):
         # data elements with no children
         self.hourly_values = []
 
-
     def __repr__(self):
         return f"DaySchedulePD(u_name='{self.u_name}')"
 
-
-
+    def populate_data_elements(self):
+        """Populate data elements that originate from eQUEST's DAY-SCHEDULE-PD command"""
+        day_sch_type = self.keyword_value_pairs.get("TYPE")
+        if day_sch_type != "RESET-TEMP" and day_sch_type != "RESET-RATIO":
+            day_sch_values_list = self.keyword_value_pairs.get("VALUES")
+            day_sch_values_list = [self.try_float(val) for val in day_sch_values_list]
+            self.hourly_values = day_sch_values_list
--- rpd_generator/bdl_structure/bdl_commands/week_schedule_pd.py
+++ rpd_generator/bdl_structure/bdl_commands/week_schedule_pd.py
@@ -9,5 +9,17 @@ class WeekSchedulePD(BaseDefinition):
     def __init__(self, u_name, rmd):
         super().__init__(u_name, rmd)
 
+        # data elements with no children
+        self.day_hourly_values = []
+
     def __repr__(self):
         return f"WeekSchedulePD(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements that originate from eQUEST's WEEK-SCHEDULE-PD command"""
+        """Create a list of 12 lists including Mon-Sun, Holidays and then 4 design day schedules"""
+        wk_sch_type = self.keyword_value_pairs.get("TYPE")
+        if wk_sch_type != "RESET-TEMP" and wk_sch_type != "RESET-RATIO":
+            wk_sch_day_sch_list = self.keyword_value_pairs.get("DAY-SCHEDULES")
+            self.day_hourly_values = [self.rmd.bdl_obj_instances[val].hourly_values for val in wk_sch_day_sch_list]
+
