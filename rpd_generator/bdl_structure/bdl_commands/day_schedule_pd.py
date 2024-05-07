02fb14551e2b293f4a5e1158b973b4eec7254309
Christina LaPerle <christina@karpmanconsulting.net>
2024-05-07 09:47:57 -0400

    Testing for troubleshooting

--- rpd_generator/bdl_structure/bdl_commands/day_schedule_pd.py
+++ rpd_generator/bdl_structure/bdl_commands/day_schedule_pd.py
@@ -9,5 +9,18 @@ class DaySchedulePD(BaseDefinition):
     def __init__(self, u_name, rmd):
         super().__init__(u_name, rmd)
 
+        # data elements with no children
+        self.hourly_values = []
+
+
     def __repr__(self):
         return f"DaySchedulePD(u_name='{self.u_name}')"
+
+    def populate_data_elements(self):
+        """Populate data elements that originate from eQUEST's DAY-SCHEDULE-PD command"""
+        """Does not appear the type is needed, REMOVE after confirming, may need to add something for reset schedules"""
+        day_sch_type = self.keyword_value_pairs.get("TYPE")
+        day_sch_values_list = self.keyword_value_pairs.get("VALUES")
+        day_sch_values_list = [self.try_float(val) for val in day_sch_values_list]
+        self.hourly_values = day_sch_values_list
+
