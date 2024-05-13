9eacbe341e9eac76840005c537324c03fa0c8543
Christina LaPerle <christina@karpmanconsulting.net>
2024-05-13 11:51:28 -0400

    Ready for review

--- rpd_generator/main.py
+++ rpd_generator/main.py
@@ -219,6 +219,6 @@ def _process_command_group(
 #
 # generate_rpd_json(
 #     [
-#         r"C:\Users\JacksonJarboe\Desktop\Active Work Container\229 Test Case 1 PSZHP_temp.BDL"
+#         r"C:\Users\chris\Karpman Consulting Dropbox\Christina LaPerle\DOE Infrastructure Bill\Tasks\Task 2.0 - RPD Gen\2.1 eQUEST\02 Testing\229P Test Models\229P Test Case 1 PSZHP\APP TEST FILES\229 Test Case 1 PSZHP_temp.BDL"
 #     ]
 # )
