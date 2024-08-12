e833efe09fa77acfc77655aa579e88a52580c4dd
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-12 17:45:30 -0400

    black format

--- rpd_generator/config.py
+++ rpd_generator/config.py
@@ -5,6 +5,7 @@ class Config:
     """
     Class to store a user's file paths for various files referenced by the program
     """
+
     EQUEST_INSTALL_PATH = None
     DOE22_DATA_PATH = None
     DOE23_DATA_PATH = None
@@ -13,7 +14,7 @@ class Config:
     RULESETS = {
         "ASHRAE 90.1-2019": {
             "enum_filename": "Enumerations2019ASHRAE901.schema.json",
-            "output_filename": "Output2019ASHRAE901.schema.json"
+            "output_filename": "Output2019ASHRAE901.schema.json",
         }
     }
 
@@ -24,6 +25,5 @@ class Config:
             Config.ACTIVE_RULESET = Ruleset(
                 name=ruleset_name,
                 enum_filename=ruleset.get("enum_filename"),
-                output_filename=ruleset.get("output_filename")
+                output_filename=ruleset.get("output_filename"),
             )
-
