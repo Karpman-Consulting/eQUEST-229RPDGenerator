ff6a97c35485fa48eb39050171a027aea985c802
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-12 17:37:28 -0400

    allow schema imports to complete before SchemaEnums

--- rpd_generator/ruleset_project_description.py
+++ rpd_generator/ruleset_project_description.py
@@ -1,15 +1,13 @@
-from rpd_generator.doe2_file_readers import model_input_reader
-
 OUTPUT_SCHEMA_ASHRAE901_2019 = "OUTPUT_SCHEMA_ASHRAE901_2019"
 
 
 class RulesetProjectDescription:
     """
-    This class is used to store project-level data, including the main rpd_data_structure that is output to json.
+    This class is used to represent the RulesetProjectDescription object in the 229 schema. It may also store additional project-level data.
     """
 
     # BDL Command Dictionary maps BDL commands to their corresponding class in the bdl_commands package
-    bdl_command_dict = model_input_reader.ModelInputReader.bdl_command_dict
+    bdl_command_dict = None
 
     def __init__(self):
 
@@ -18,7 +16,7 @@ class RulesetProjectDescription:
         # data elements with children
         self.ruleset_model_descriptions = []
         self.calendar = {
-            "is_leap_year": False,
+            "is_leap_year": False,  # even if the year is a leap year, eQUEST skips Feb 29
         }
         self.weather = {}
         self.ground_temperature_schedule = None
@@ -35,9 +33,6 @@ class RulesetProjectDescription:
         self.data_version = None
         self.compliance_path = None
         self.output_format_type = OUTPUT_SCHEMA_ASHRAE901_2019
-
-    def populate_data_elements(self):
-        """Populate the data elements in the RPD data group."""
 
     def populate_data_group(self):
         """
@@ -62,4 +57,4 @@ class RulesetProjectDescription:
         for attr in no_children_attributes:
             value = getattr(self, attr, None)
             if value is not None:
-                self.rpd_data_structure["ASHRAE 229"][attr] = value
+                self.rpd_data_structure[attr] = value
