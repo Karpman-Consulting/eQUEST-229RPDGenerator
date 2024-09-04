4a28ae10cfb111a3faeab87dd7085c58041aaa39
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-09-04 00:12:16 -0400

    troubleshoot workflow

--- test/generate_test_rpds_from_bdls.py
+++ test/generate_test_rpds_from_bdls.py
@@ -1,4 +1,8 @@
+import sys
 from pathlib import Path
+project_root = Path(__file__).resolve().parents[1]
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
 from rpd_generator import main as rpd_generator
 from rpd_generator.utilities import validate_configuration
 
