ccdafe7f58e0befcdf0912fef30d15261c4bc4f7
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-09-20 17:55:41 -0400

    troubleshoot path

--- test/populate_test/run_tests.py
+++ test/populate_test/run_tests.py
@@ -1,8 +1,10 @@
 import unittest
+import os
 
-
+current_dir = os.path.dirname(os.path.abspath(__file__))
+start_dir = os.path.join(current_dir, 'bdl_commands_test')
 loader = unittest.TestLoader()
-suite = loader.discover(start_dir="./bdl_commands_test", pattern="*_test.py")
+suite = loader.discover(start_dir=start_dir, pattern="*_test.py")
 
 runner = unittest.TextTestRunner()
 runner.run(suite)
