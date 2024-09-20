f87cecdaca275bfc150060eb1420bf58f673926b
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-09-20 17:57:35 -0400

    clean up

--- test/populate_test/run_tests.py
+++ test/populate_test/run_tests.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-start_dir = os.path.join(current_dir, 'bdl_commands_test')
+start_dir = os.path.join(current_dir, "bdl_commands_test")
 loader = unittest.TestLoader()
 suite = loader.discover(start_dir=start_dir, pattern="*_test.py")
 
