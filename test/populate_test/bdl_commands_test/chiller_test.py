e230d4f41ce08e4097c02181a5c0bdd46a47a4c0
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-09-21 22:41:24 -0400

    add chiller unit test

---
+++ test/populate_test/bdl_commands_test/chiller_test.py
@@ -0,0 +1,67 @@
+import unittest
+import os
+from rpd_generator.config import Config
+from rpd_generator.utilities import validate_configuration
+from rpd_generator.schema.schema_enums import SchemaEnums
+
+Config.set_active_ruleset("ASHRAE 90.1-2019")
+SchemaEnums.update_schema_enum(Config.ACTIVE_RULESET)
+validate_configuration.find_equest_installation()
+
+from rpd_generator.artifacts.ruleset_model_description import RulesetModelDescription
+from rpd_generator.bdl_structure.bdl_commands.chiller import Chiller, BDL_ChillerTypes
+from rpd_generator.bdl_structure.bdl_enumerations.bdl_enums import BDLEnums
+
+BDL_Commands = BDLEnums.bdl_enums["Commands"]
+BDL_ChillerKeywords = BDLEnums.bdl_enums["ChillerKeywords"]
+EnergySourceOptions = SchemaEnums.schema_enums["EnergySourceOptions"]
+
+
+class TestElectricChillers(unittest.TestCase):
+    def setUp(self):
+        self.rmd = RulesetModelDescription("Test RMD")
+        self.rmd.doe2_version = "DOE-2.3"
+        self.rmd.doe2_data_path = Config.DOE23_DATA_PATH
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.rmd.file_path = os.path.abspath(
+            os.path.join(
+                script_dir, "../../full_rpd_test/E-2/229 Test Case E-2 (CHW VAV)"
+            )
+        )
+        self.chiller = Chiller("Chiller 1", self.rmd)
+
+    def test_populate_data_elements_with_centrif_chiller(self):
+        self.chiller.keyword_value_pairs = {
+            BDL_ChillerKeywords.TYPE: BDL_ChillerTypes.ELEC_OPEN_CENT,
+            BDL_ChillerKeywords.CHW_LOOP: "Chilled Water Loop (Primary)",
+            BDL_ChillerKeywords.CW_LOOP: "Condenser Water Loop",
+            BDL_ChillerKeywords.RATED_CHW_T: "44",
+            BDL_ChillerKeywords.RATED_COND_T: "85",
+            BDL_ChillerKeywords.DESIGN_CHW_T: "50",
+            BDL_ChillerKeywords.DESIGN_COND_T: "70",
+        }
+
+        self.chiller.populate_data_elements()
+        self.chiller.populate_data_group()
+
+        expected_data_structure = {
+            "id": "Chiller 1",
+            "compressor_type": "CENTRIFUGAL",
+            "energy_source_type": "ELECTRICITY",
+            "cooling_loop": "Chilled Water Loop (Primary)",
+            "condensing_loop": "Condenser Water Loop",
+            "design_entering_condenser_temperature": 70.0,
+            "design_leaving_evaporator_temperature": 50.0,
+            "rated_entering_condenser_temperature": 85.0,
+            "rated_leaving_evaporator_temperature": 44.0,
+            "rated_capacity": 125344.671875,
+            "design_capacity": 158091.265625,
+            "design_flow_condenser": 37.681522369384766,
+            "design_flow_evaporator": 30.145217895507812,
+            "capacity_validation_points": [],
+            "power_validation_points": [],
+            "part_load_efficiency": [],
+            "part_load_efficiency_metrics": [],
+        }
+
+        self.assertEqual(self.chiller.chiller_data_structure, expected_data_structure)
