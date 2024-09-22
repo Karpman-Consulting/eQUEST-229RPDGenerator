b3fcea5d7a4f65d1b057143966bf653d6c9453f7
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-09-21 22:31:26 -0400

    test pre commit

--- test/populate_test/run_tests.py
+++ test/populate_test/run_tests.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import sys
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 start_dir = os.path.join(current_dir, "bdl_commands_test")
@@ -7,4 +9,7 @@ loader = unittest.TestLoader()
 suite = loader.discover(start_dir=start_dir, pattern="*_test.py")
 
 runner = unittest.TextTestRunner()
-runner.run(suite)
+result = runner.run(suite)
+
+if not result.wasSuccessful():
+    sys.exit(1)
