524b042986e91adab2f298754d2f4dbaed6b410a
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-02-28 10:05:42 -0500

    fan operation corrections

--- rpd_generator/bdl_structure/bdl_commands/system.py
+++ rpd_generator/bdl_structure/bdl_commands/system.py
@@ -1494,14 +1494,18 @@ class System(ParentNode):
                 )
             if is_all_1:
                 self.fan_sys_operation_during_unoccupied = (
-                    FanSystemOperationOptions.KEEP_OFF
+                    self.occupied_fan_operation_map.get(
+                        self.get_inp(BDL_SystemKeywords.INDOOR_FAN_MODE)
+                    )
                 )
 
             mixed_operation = has_one and has_neg_999
             if mixed_operation:
                 return FanSystemOperationOptions.OTHER
             if has_one:  # and not mixed_operation implied to reach here
-                return FanSystemOperationOptions.CONTINUOUS
+                return self.occupied_fan_operation_map.get(
+                    self.get_inp(BDL_SystemKeywords.INDOOR_FAN_MODE)
+                )
             if has_neg_999:  # and not mixed_operation implied to reach here
                 return FanSystemOperationOptions.CYCLING
 
@@ -1514,7 +1518,7 @@ class System(ParentNode):
                     )
                 if all(value == 1 for value in fan_sch.hourly_values):
                     self.fan_sys_operation_during_unoccupied = (
-                        FanSystemOperationOptions.KEEP_OFF
+                        FanSystemOperationOptions.CONTINUOUS
                     )
 
                 if any(value == -999 for value in fan_sch.hourly_values):
@@ -1532,9 +1536,8 @@ class System(ParentNode):
     def get_doas_occ_sch(self):
         systems_served = [
             obj_inst
-            for obj_inst in self.rmd.bdl_obj_instances
-            if isinstance(obj_inst, System)
-            and obj_inst.get_inp(BDL_SystemKeywords.DOA_SYSTEM) == self.u_name
+            for obj_inst in list(map(self.get_obj, self.rmd.system_names))
+            if obj_inst.get_inp(BDL_SystemKeywords.DOA_SYSTEM) == self.u_name
         ]
         system_fan_schedules = {
             self.get_obj(system.get_inp(BDL_SystemKeywords.FAN_SCHEDULE))
