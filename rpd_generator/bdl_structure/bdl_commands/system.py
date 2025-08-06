769dd6d0bd123476ee83aa0fe803b525ae5f907b
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-08-06 14:24:55 -0400

    protect against division by 0

--- rpd_generator/bdl_structure/bdl_commands/system.py
+++ rpd_generator/bdl_structure/bdl_commands/system.py
@@ -1984,10 +1984,10 @@ class CoolingSystem:
         """
         cooling_eir, cop, entering_condenser_temperature = None, None, None
 
-        if (
-            cooling_eir := self.parent_system.get_inp(BDL_SystemKeywords.COOLING_EIR)
-        ) is not None:
-            cop = 1 / self.parent_system.try_float(cooling_eir)
+        if cooling_eir := self.parent_system.get_inp(BDL_SystemKeywords.COOLING_EIR):
+            cooling_eir_val = self.parent_system.try_float(cooling_eir)
+            if cooling_eir_val not in (None, 0):
+                cop = 1 / cooling_eir_val
 
         if (
             rated_ect := self.parent_system.get_inp(BDL_SystemKeywords.RATED_ECT)
@@ -2018,10 +2018,7 @@ class CoolingSystem:
             else:
                 self.efficiency_metric_types.append(CoolingMetricOptions.OTHER)
 
-            if (
-                self.vrf_sys_condenser.get_inp(BDL_CondenserKeywords.COOLING_EIR)
-                is not None
-            ):
+            if self.vrf_sys_condenser.get_inp(BDL_CondenserKeywords.COOLING_EIR):
                 self.efficiency_metric_values.append(
                     1
                     / self.parent_system.try_float(
